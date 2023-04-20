e8bd0fe70b02602f248f068537341ff316e33aee
georgestathis <georgestathis1996@yahoo.gr>
2023-04-20 21:34:13 +0300

    Initialize DbMigration Create category Model Add migration to add category table to Db

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace BulkyWeb.Controllers
         public IActionResult Privacy()
         {
             return View();
+            //return View("Privacy");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
